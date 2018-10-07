5425840c1bf6b1852d5ebfc7c1028e87a249ef44
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-07 13:40:58 +0530

    login 1

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -37,13 +37,30 @@ class Users extends CI_Controller
         $this->form_validation->set_rules('password', 'Password','trim|required|min_length[3]');
         $this->form_validation->set_rules('confirm_password', 'Confirm Password','trim|required|min_length[3]|match[password]');
 
-        
+
         if($this->form_validation->run() == FALSE){
             $data = array(
                 'errors' => validation_errors()
             );
             $this->session->set_flashdata($data);
             redirect('home');
+        }else{
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            $this->user_model->login_user($username,$password);
+
+            if($user_id){
+                $user_data = array(
+                    'user_id' => $user_id,
+                    'username' => $username,
+                    'logged_in' => true
+                );
+                $this->session->set_userdata($user_data);
+                redirect('home/index');
+            }else{
+                redirect('home/index');
+            }
         }
 
     }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,32 +1,46 @@
 <?php 
 class User_model extends CI_Model
 {
-    public function get_users($id)
-    {
-     $this->db->where('id',$id);   
-        $query =  $this->db->get('user');
-        return $query->result();
-    }
+    // public function get_users($id)
+    // {
+    //  $this->db->where('id',$id);   
+    //     $query =  $this->db->get('user');
+    //     return $query->result();
+    // }
 
-    public function create_user($data)
-    {
-        # code...
-        $this->db->insert('user',$data);
-    }
+    // public function create_user($data)
+    // {
+    //     # code...
+    //     $this->db->insert('user',$data);
+    // }
 
-    public function update_user($data,$id)
-    {
-        # code...
-        $this->db->where(['id' => $id]);
-        $this->db->update('user',$data);
-    }
+    // public function update_user($data,$id)
+    // {
+    //     # code...
+    //     $this->db->where(['id' => $id]);
+    //     $this->db->update('user',$data);
+    // }
 
-    public function delete_user($id)
-    {
-        # code...
+    // public function delete_user($id)
+    // {
+    //     # code...
         
-        $this->db->where(['id' => $id]);
-        $this->db->delete('user');
+    //     $this->db->where(['id' => $id]);
+    //     $this->db->delete('user');
+    // }
+
+    public function login_user($username,$password)
+    {
+        $this->db->where('username',$username);
+        $this->db->where('username',$password);
+
+        $result = $this->db->get('user');
+
+        if($result->num_rows() == 1){
+            return $result->row(0)->$id;
+        }else{
+            return false;
+        }
     }
     
 }
