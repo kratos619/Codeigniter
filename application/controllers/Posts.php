09edb8eed0d5bcd5f56a5888f9c5a6da0c126e0e
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 11:17:44 +0530

    load posts controller load  data with view

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -4,9 +4,26 @@ class Posts extends CI_Controller
     public function index()
     {
         $data['title'] = 'Latest Posts';
+
+        $data['posts'] = $this->post_model->get_post();
+
+      //  print_r($data['posts']);
+
         $this->load->view('layouts/header');
         $this->load->view('posts/index',$data);
         $this->load->view('layouts/footer');
 
+    }
+
+    public function view($slug = null){
+        $data['post'] = $this->post_model->get_post($slug);
+
+        if(empty($data['post'])){
+            show_404();
+        }
+        $data['title'] = $data['post']['title'];
+        $this->load->view('layouts/header');
+        $this->load->view('posts/view',$data);
+        $this->load->view('layouts/footer');
     }
 }
