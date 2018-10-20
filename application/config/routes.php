797f6dacb0ac73d488fd3f88b99e93eb5bc7e813
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 17:46:53 +0530

    config route to update page

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['posts/(:any)'] = 'posts/view/$1';
 $route['posts'] = 'posts/index';
+$route['posts/update'] = 'posts/update';
 $route['posts/create'] = 'posts/create';
 $route['default_controller'] = 'pages/view';
 
