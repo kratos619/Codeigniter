08b3021c45e0a6618380647d91af2ad37b61ea8b
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 18:37:36 +0530

    display create categories to post edite form

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -11,12 +11,19 @@
     <div class="form-group">
       <label for="title">Title</label>
       <input value="<?php echo $post['title'] ?>" class="form-control" name="title" aria-describedby="title" placeholder="title" type="text">
-
     </div>
     <!-- <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
       <input class="form-control" id="exampleInputPassword1" placeholder="Password" type="password">
     </div> -->
+    <div class="form-group">
+      <label for="exampleSelect1">Select Categories</label>
+      <select name="categories_id" class="form-control" id="selectcat">
+        <?php foreach($categories as $cat): ?>
+        <option value="<?php echo $cat['id']; ?>" ><?php echo $cat['name']; ?></option>
+        <?php endforeach ?>
+      </select>
+    </div>
     <div class="form-group">
       <label for="exampleTextarea">Body</label>
       <textarea  class="form-control" name="body" id="body" rows="6">
