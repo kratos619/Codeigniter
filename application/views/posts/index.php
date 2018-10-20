76cdc8f0e9a3d246e5504e383fb9f40355e56828
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 18:37:46 +0530

    update view

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,6 +1,8 @@
 <h2><?= $title?></h2>
 <?php foreach($posts as $post): ?>
 <h3><?php echo $post['title'] ?></h3>
+<hr>
+<p class="text-muted"><small><strong class="badge badge-pill badge-primary">In: <?php echo $post['name']; ?>, Posted On: <?php echo $post['created_at']; ?></strong></small></p>
 <p class="lead"><?php echo word_limiter( $post['body'],50); ?></p>
 <p class="text-muted"><a href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
 <?php endforeach; ?>
