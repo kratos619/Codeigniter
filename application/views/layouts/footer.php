0f843710c3a2b0185d2bf828daced39500448794
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 09:57:07 +0530

    add header and footer new file to layouts folder

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,6 @@
+    
+    </div>
+    <script src="<?php echo base_url(); ?>/assets/css/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/css/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ application/views/layouts/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+
+    <title>Document</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">Ci Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<body>
+<div class="container">
