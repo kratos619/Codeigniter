2bec922d64dc14eaffa6c484a7089a510c3a6d56
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 11:18:02 +0530

    load post model

---
+++ application/models/Post_model.php
@@ -0,0 +1,16 @@
+<?php   
+class Post_model extends CI_Model{
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function get_post($slug = false){
+        if($slug === false){
+            $query = $this->db->get('posts');
+            return $query->result_array();
+        }
+        $query = $this->db->get_where('posts', array('slug' => $slug));
+        return $query->row_array();
+
+    }
+}
