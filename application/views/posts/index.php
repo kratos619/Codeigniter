48dcf11695828075bc7e723427b13608f9839c86
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 17:55:28 +0530

    set ford limiter

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,7 +1,7 @@
 <h2><?= $title?></h2>
 <?php foreach($posts as $post): ?>
 <h3><?php echo $post['title'] ?></h3>
-<p class="lead"><?php echo $post['body']; ?></p>
+<p class="lead"><?php echo word_limiter( $post['body'],50); ?></p>
 <p class="text-muted"><a href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
 <?php endforeach; ?>
 <hr>
