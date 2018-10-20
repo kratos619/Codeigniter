c132a1162407ee25d4e265d7d2c78a674c31e065
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 09:56:17 +0530

    create view method to load home page

---
+++ application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php 
+class Pages extends CI_Controller
+{
+    public function view($page = 'home')
+    {
+        if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page);
+        $this->load->view('layouts/header');
+        $this->load->view('pages/'.$page,$data);
+        $this->load->view('layouts/footer');
+
+    }
+}
