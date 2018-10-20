2ab8ea7020444c5498f76579ab69ad8c2ea4a5b5
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 09:56:44 +0530

    break main.php to header and footer

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
-
-    <title>Document</title>
-</head>
-<body>
     <div class="container">
         <div class="col-xs-3">
         <?php $this->load->view('users/login_view') ?>
@@ -17,7 +6,3 @@
          <?php $this->load->view($main_view) ?>
         </div>
     </div>
-    <script src="<?php echo base_url(); ?>/assets/css/jquery.js"></script>
-    <script src="<?php echo base_url(); ?>/assets/css/bootstrap.min.js"></script>
-</body>
-</html>
