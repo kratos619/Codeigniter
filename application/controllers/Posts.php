68014ad8e8e6af90cdb252d748e2d2d07113fabd
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 12:24:25 +0530

    create validation  for title and body

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -30,8 +30,19 @@ class Posts extends CI_Controller
     public function create()
     {
         $data['title'] = 'create Post';
+        $this->form_validation->set_rules('title','Title','required');
+        $this->form_validation->set_rules('body','Body','required');
+
+        if($this->form_validation->run() === false){
+            
         $this->load->view('layouts/header');
         $this->load->view('posts/create',$data);
         $this->load->view('layouts/footer');
+
+        }else{
+            $this->post_model->create_post();
+           redirect('posts');
+        }
+
     }
 }
