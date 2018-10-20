7549a97d1ea96e2f0e88771f43197060e13f3e15
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 12:38:28 +0530

    add form to delete post to view

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,2 +1,5 @@
 <h2><?php echo $post['title'] ?></h2>
 <p class="lead"><?php echo $post['body'] ?></p>
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+<input type="submit" class="btn btn-danger" value="delete">
+<?php echo form_close(); ?>
