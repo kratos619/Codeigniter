9e108da7ba5eba24122a03f88c7cb3ce742f1103
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 11:18:57 +0530

    load view / to show posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,1 +1,6 @@
 <h2><?= $title?></h2>
+<?php foreach($posts as $post): ?>
+<h3><?php echo $post['title'] ?></h3>
+<p class="lead"><?php echo $post['body']; ?></p>
+<p class="text-muted"><a href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
+<?php endforeach; ?>
