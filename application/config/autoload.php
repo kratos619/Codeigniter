ed88831d9639a4b88966e1c93611c18874644eed
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 11:15:44 +0530

    load user_mode

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -126,4 +126,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('user_model');
+$autoload['model'] = array('post_model');
