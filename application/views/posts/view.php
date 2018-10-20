7843e29658a1b867cc820d0bcf2e0040767d3e1b
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-20 17:48:39 +0530

    .

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,5 +1,8 @@
 <h2><?php echo $post['title'] ?></h2>
 <p class="lead"><?php echo $post['body'] ?></p>
+<hr>
 <?php echo form_open('/posts/delete/'.$post['id']); ?>
 <input type="submit" class="btn btn-danger" value="delete">
 <?php echo form_close(); ?>
+<hr>
+<a href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>" class="btn btn-warning">edit</a>
