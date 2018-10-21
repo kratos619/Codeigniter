9737492c7597d0fedd1a808aaf6cb532fbd0960f
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-21 17:35:16 +0530

    config routes for categories

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,7 @@ $route['posts/(:any)'] = 'posts/view/$1';
 $route['posts'] = 'posts/index';
 $route['posts/update'] = 'posts/update';
 $route['posts/create'] = 'posts/create';
+$route['categories/create'] = 'categories/create';
 $route['default_controller'] = 'pages/view';
 
 $route['(:any)'] = 'pages/view/$1';
