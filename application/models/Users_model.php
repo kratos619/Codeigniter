a40d7e16a231f1984c7fb0fec4c01c40889a9abd
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-24 18:22:29 +0530

    create method to register user to db

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -2,4 +2,17 @@
 
 class Users_model extends CI_Model{
     
+    public function __construct(){
+        $this->load->database();
+    }
+    public function register_new_user($password){
+        $data = array(
+            'name' => $this->input->post('name'),
+              'email' => $this->input->post('email'),
+              'username' => $this->input->post('username'),  
+              'password' => $password        
+        );
+
+        return $this->db->insert('register_user',$data); 
+    }
 }
