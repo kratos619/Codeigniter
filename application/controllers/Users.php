cadaeaf32dc6ba80fefc92df6c597d38dac17d31
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-24 21:20:16 +0530

    set validation to check exists username and email

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -4,8 +4,8 @@ class Users extends CI_Controller{
     public function register(){
         $data['title']  = "Register User";
             $this->form_validation->set_rules('name','Your Name','required');
-    $this->form_validation->set_rules('email','Email','required');
-    $this->form_validation->set_rules('username','Username','required');
+    $this->form_validation->set_rules('email','Email','required|callback_check_email_exists');
+    $this->form_validation->set_rules('username','Username','required|callback_check_username_exists');
     $this->form_validation->set_rules('password','Password','required');
     $this->form_validation->set_rules('matchpassword','Confirm Password','matches[password]');
         if($this->form_validation->run() === false){
@@ -28,6 +28,36 @@ class Users extends CI_Controller{
                     'You are Registered and can login'
                 );
               redirect('posts');
+        }
+    }
+
+    //check if username exists
+    public function check_username_exists($username)
+    {
+        $this->form_validation->set_message(
+        'check_username_exists',
+        'that Username is already Taken Please Select Other One'
+        );
+
+        if($this->users_model->check_username_exists($username)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //check if email is exists
+    public function check_email_exists($email)
+    {
+        $this->form_validation->set_message(
+            'check_email_exists',
+            'Email Is alredy Registered'
+        );
+
+        if($this->users_model->check_email_exists($email)){
+            return true;
+        }else{
+            return false;
         }
     }
 }
--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -15,4 +15,28 @@ class Users_model extends CI_Model{
 
         return $this->db->insert('register_user',$data); 
     }
+
+    public function check_username_exists($username){
+        $query = $this->db->get_where('register_user' ,array(
+            'username' => $username
+        ));
+
+        if(empty($query->row_array())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function check_email_exists($email){
+        $query = $this->db->get_where('register_user' ,array(
+            'email' => $email
+        ));
+
+        if(empty($query->row_array())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
